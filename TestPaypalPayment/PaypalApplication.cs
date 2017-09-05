6d43967d69f5139996c34b820572b54822cb27c3
ky ho <ky ho>
2017-09-05 15:06:19 +0700

    [Init] Project

---
+++ TestPaypalPayment/PaypalApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Com.Facebook.Stetho;
+using Square.LeakCanary;
+namespace TestPaypalPayment
+{
+	[Application(Theme = "@style/AppTheme")]
+	public class PaypalApplication : Application
+	{
+		public PaypalApplication(IntPtr handle, global::Android.Runtime.JniHandleOwnership transer) : base(handle, transer)
+		{
+		}
+
+		public override void OnCreate()
+		{
+			base.OnCreate();
+			LeakCanaryXamarin.Install(this);
+			Stetho.InitializeWithDefaults(this);
+		}
+	}
+}
